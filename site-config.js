5bbfcaaad27a06cca939434b13cf269edb9eb333
jeff vax <jefffvasss@gmail.com>
2026-05-04 18:07:45 +0100

    Create site-config.js

---
+++ site-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteName: "9ja News",
+  baseUrl: "https://blog.pluse.name.ng",
+  description: "Latest news, politics, sports and trending stories from Nigeria and beyond.",
+
+  language: "en",
+
+  author: {
+    name: "Ifeanyi Clement Okoye",
+    url: "https://blog.pluse.name.ng/authors/ifeanyi-okoye",
+    jobTitle: "Editor"
+  }
+};
